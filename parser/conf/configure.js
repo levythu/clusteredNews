476d5bf6e525d4aa457f8dd3ac5f6e595a937aeb
Levy <levy@levys.ink>
2015-12-19 01:15:51 +0800

    add comments on configure for parser

--- parser/conf/configure.js
+++ parser/conf/configure.js
@@ -1,12 +1,16 @@
+// Global configure for parser. Export the whole configure info by an object.
+
 module.exports=
 {
     database:
     {
+        // the schema used to access mongodb.
         access_schema: "mongodb://localhost/cnews"
     },
     worker:
     {
         // if whitelist exist, blacklist is invalid.
+        // lists are provided in REGEXP
         blacklist: [],
         whitelist:
         [
@@ -15,13 +19,18 @@ module.exports=
     },
     scheduler:
     {
-        // max worker working at the same time.
+        // max worker working at the same time, when batching workers, the number of living
+        // workers will be supplemented to this value.
         max_worker: 1,
 
+        // if launch the crawler in entry.js, the time indicates the interval (in seconds) of
+        // each batch.
         spawn_interval_in_seconds: 60
     },
     log:
     {
+        // if the value>0, when the number of workers can be divided by the value or reached max, the event is logged.
+        // if the value=0, the number of workers will not trigger any logging.
         log_workers_num: 1
     }
 }
