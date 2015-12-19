592ef27765f239b89c0e84428dc6e53d1f014a6e
Levy <levy@levys.ink>
2015-12-19 15:02:33 +0800

    complete cruiser

--- cruiser/conf/configure.js
+++ cruiser/conf/configure.js
@@ -17,7 +17,12 @@ module.exports=
         // cruiser checks all the locked entries and unlock it in a period to avoid dead lock or crawler/parser crash.
         // please guarantee that almost all the normal crawlers and parsers can done their jobs in the expiration time.
         // if value<0, deadlock chead will be disabled. IT IS STRONGLY NOT RECOMMENDED TO DO SO.
-        locked_html_expire_in_seconds: 5*60
+        locked_html_expire_in_seconds: 5*60,
+
+        // for those temporarily unavailable pages (network error, server internal error, etc.)
+        // the crawlers can refetch them after the time period.
+        // if value<0, it will never be refetched again.
+        refetch_temporarily_unavailable_pages_in_seconds: 24*60*60
     },
     scheduler:
     {
--- cruiser/kernel/worker.js
+++ cruiser/kernel/worker.js
@@ -31,8 +31,109 @@ module.exports=batchWorkers;
 
 function scanOutdatedDocs(callback)
 {
+    var tscale=conf.worker.fetched_html_expire_in_seconds;
+    if (tscale<0)
+    {
+        process.nextTick(callback);
+        return;
+    }
+    var current=Date.now();
+    var outdatedBefore=current-tscale*1000;
+    db[RAWHTML].update(
+    {
+        fetchtime: {$lt: outdatedBefore},
+        $or: [{status: 2}, {status: 3}]
+    },
+    {
+        $set: {status: 1}
+    }, {multi: true}, function(err, result)
+    {
+        if (err)
+            console.log((new Date()).toUTCString(), ">\tFail to update outdated pages.");
+        else
+            console.log((new Date()).toUTCString(), ">\tUpdate "+result.n+" outdated pages.");
+        process.nextTick(callback);
+    });
+}
+function unlockEntries(callback)
+{
+    var tscale=conf.worker.locked_html_expire_in_seconds;
+    if (tscale<0)
+    {
+        process.nextTick(callback);
+        return;
+    }
+    var current=Date.now();
+    var outdatedBefore=current-tscale*1000;
+    db[RAWHTML].update(
+    {
+        status: 999,
+        locktime: {$lt: outdatedBefore},
+        raw: {$exists: true}
+    },
+    {
+        $set: {status: 2}
+    }, {multi: true}, function(err, result)
+    {
+        if (err)
+            console.log((new Date()).toUTCString(), ">\tFail to unlock on-parsing pages.");
+        else
+            console.log((new Date()).toUTCString(), ">\tUnlock "+result.n+" on-parsing pages.");
 
+        db[RAWHTML].update(
+        {
+            status: 999,
+            locktime: {$lt: outdatedBefore},
+            raw: {$exists: false}
+        },
+        {
+            $set: {status: 0}
+        }, {multi: true}, function(err, result)
+        {
+            if (err)
+                console.log((new Date()).toUTCString(), ">\tFail to unlock on-fetching pages.");
+            else
+                console.log((new Date()).toUTCString(), ">\tUnlock "+result.n+" on-fetching pages.");
+            process.nextTick(callback);
+        });
+    });
+}
+function refetchBroken(callback)
+{
+    var tscale=conf.worker.refetch_temporarily_unavailable_pages_in_seconds;
+    if (tscale<0)
+    {
+        process.nextTick(callback);
+        return;
+    }
+    var current=Date.now();
+    var outdatedBefore=current-tscale*1000;
+    db[RAWHTML].update(
+    {
+        status: 901,
+        fetchtime: {$lt: outdatedBefore}
+    },
+    {
+        $set: {status: 0}
+    }, {multi: true}, function(err, result)
+    {
+        if (err)
+            console.log((new Date()).toUTCString(), ">\tFail to reload broken pages.");
+        else
+            console.log((new Date()).toUTCString(), ">\tReload "+result.n+" broken pages.");
+        process.nextTick(callback);
+    });
 }
 function work()
 {
+    unlockEntries(function()
+    {
+        refetchBroken(function()
+        {
+            scanOutdatedDocs(function()
+            {
+                reportDeath();
+            });
+        });
+    });
 }
